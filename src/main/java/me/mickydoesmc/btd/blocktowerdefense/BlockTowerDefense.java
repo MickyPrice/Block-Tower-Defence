09792abcd86a5300b151694146c6817b4e9fc01e
Michael Price <realmprice@gmail.com>
2021-01-02 20:44:56 +1300

    Initial Setup

---
+++ src/main/java/me/mickydoesmc/btd/blocktowerdefense/BlockTowerDefense.java
@@ -0,0 +1,17 @@
+package me.mickydoesmc.btd.blocktowerdefense;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockTowerDefense extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/mickydoesmc/btd/blocktowerdefense/Events/BTDTickEvent.java
@@ -0,0 +1,11 @@
+package me.mickydoesmc.btd.blocktowerdefense.Events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class BTDTickEvent extends Event {
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
